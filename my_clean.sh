c6a9b579898525c8aa1680109aaa31513173cff2
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-03-27 21:59:11 +0100

    first commit

---
+++ my_clean.sh
@@ -0,0 +1,6 @@
+find . -name "*~" -o -name "#*#" 
+# Description
+# In a file called my_clean place the command line that will search for all files - in the current directory as well as in its sub-directories - with a name ending by ~, or a name that start and end by #
+
+# The command line will print and delete all files found.
+# Only one command is allowed: no ; or && or other gitaneries.
